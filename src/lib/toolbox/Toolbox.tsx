43bc3a61d2e3018edcd423d28ff47982dc278237
jbailey111 <jake.bailey2599@gmail.com>
2021-12-23 22:03:05 +0000

    Created toolbox skeleton

--- src/lib/toolbox/Toolbox.tsx
+++ src/lib/toolbox/Toolbox.tsx
@@ -1,9 +1,22 @@
-import { ToolboxWrapper } from "./ToolboxElements"
+import { ToolboxContainer, ToolboxHeader, ToolboxTitle, ToolboxWrapper, ToolboxItem, ToolboxItemList, ToolboxComponent } from "./ToolboxElements"
 
 const Toolbox = () => {
     return (
         <div>
-            <ToolboxWrapper />
+            <ToolboxWrapper>
+                <ToolboxHeader>
+                    <ToolboxTitle>
+                    Toolbox
+                    </ToolboxTitle>
+                </ToolboxHeader>
+                <ToolboxContainer>
+                    <ToolboxItemList>
+                    <ToolboxItem><ToolboxComponent /></ToolboxItem>
+                    <ToolboxItem><ToolboxComponent /></ToolboxItem>
+                    <ToolboxItem><ToolboxComponent /></ToolboxItem>
+                    </ToolboxItemList>
+                </ToolboxContainer>
+            </ToolboxWrapper>
         </div>
     )
 }
--- src/lib/toolbox/ToolboxElements.ts
+++ src/lib/toolbox/ToolboxElements.ts
@@ -1,4 +1,41 @@
 import styled from "styled-components";
 
 export const ToolboxWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width:300px;
+    height: 100vh;
+    background:grey;
+`
+
+export const ToolboxHeader = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 4em;
+`
+
+export const ToolboxTitle = styled.h1`
+`
+
+export const ToolboxContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const ToolboxItemList = styled.ul`
+    list-style: none;
+`
+
+export const ToolboxItem = styled.li`
+    margin: 1em;
+    text-align: center;
+`
+
+export const ToolboxComponent = styled.div`
+    width:100%;
+    height: 100px;
+    background-color: blue;
 `
