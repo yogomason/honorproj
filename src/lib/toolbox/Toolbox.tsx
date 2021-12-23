3202f02892992d4fc04952574b3fbb7e62d78a4d
jbailey111 <jake.bailey2599@gmail.com>
2021-12-23 21:18:04 +0000

    Added styled components and base toolbox component

---
+++ src/lib/toolbox/Toolbox.tsx
@@ -0,0 +1,11 @@
+import { ToolboxWrapper } from "./ToolboxElements"
+
+const Toolbox = () => {
+    return (
+        <div>
+            <ToolboxWrapper />
+        </div>
+    )
+}
+
+export default Toolbox
