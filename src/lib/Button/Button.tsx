f79165a8d520982f2f9e7e3a53d4b0e4381ece9b
jbailey111 <jake.bailey2599@gmail.com>
2021-12-25 02:50:28 +0000

    Added basic button element

---
+++ src/lib/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonText, ButtonWrapper } from "./ButtonElements"
+
+const Button = () => {
+    return (
+        <>
+            <ButtonWrapper>
+                <ButtonText>
+                    Click Me!
+                </ButtonText>
+            </ButtonWrapper>
+        </>
+    )
+}
+
+export default Button
