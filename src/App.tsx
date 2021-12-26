2b33e4571cc1055ce6141009667f3016417e6b1e
jbailey111 <jake.bailey2599@gmail.com>
2021-12-26 13:23:08 +0000

    Added typing to Frame items

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { JsxEmit } from "typescript";
 import Button from "./lib/Button/Button";
 import Frame from "./lib/Frame/Frame";
 import Toolbox from "./lib/toolbox/Toolbox";
 
 function App() {
   const [Open, setOpen] = useState(true)
-  const [FrameItems, setFrameItems] = useState([<Button />])
+  const [FrameItems, setFrameItems] = useState<JSX.Element[]>([])
 
   const openToolbox = () => {
     setOpen(!Open)
